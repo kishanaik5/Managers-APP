648988b3c193a75a544319df66a337b2cbfb69fa
Kishan K <120300882+kishanaik5@users.noreply.github.com>
2025-02-02 20:23:44 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,4 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     window.location.href = "https://productivity-management.onrender.com/manager-login";
-// });
-
-
-document.getElementById('redirectButton').addEventListener('click', function() {
+document.addEventListener('DOMContentLoaded', function() {
     window.location.href = "https://productivity-management.onrender.com/manager-login";
 });
+
